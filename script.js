ba9dac8770b87af32c056939e9336985a4980f6c
Uteraw <162019426+Uteraw@users.noreply.github.com>
2024-12-03 03:47:10 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".container");
+    setTimeout(() => {
+      container.classList.add("loaded");
+    }, 100);
+  });
+
